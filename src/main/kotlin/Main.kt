cb53753326ef0e9f600639546a44fca97715843b
Artem <artembirmin@gmail.com>
2021-10-11 17:59:36 +0300

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,17 +5,17 @@ import org.ktorm.dsl.select
 import java.time.LocalDate
 
 fun main(args: Array<String>) {
-    val database = Database.connect("jdbc:mysql://localhost:3306/ktorm", user = "root", password = "***")
-    database.insert(Employees) {
-        set(it.name, "jerry")
-        set(it.job, "trainee")
-        set(it.managerId, 1)
-        set(it.hireDate, LocalDate.now())
-        set(it.salary, 50)
-        set(it.departmentId, 1)
-    }
-
-    for (row in database.from(Employees).select()) {
-        println(row[Employees.name])
-    }
+//    val database = Database.connect("jdbc:mysql://localhost:3306/ktorm", user = "root", password = "***")
+//    database.insert(Employees) {
+//        set(it.name, "jerry")
+//        set(it.job, "trainee")
+//        set(it.managerId, 1)
+//        set(it.hireDate, LocalDate.now())
+//        set(it.salary, 50)
+//        set(it.departmentId, 1)
+//    }
+//
+//    for (row in database.from(Employees).select()) {
+//        println(row[Employees.name])
+//    }
 }
